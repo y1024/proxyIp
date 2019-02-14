30fe73a810abafc8bf729ad22ef2b21f0659be63
YiQiang <164833661@qq.com>
2019-02-14 21:05:09 +0800

    fix 请求异常处理

--- checkIp.py
+++ checkIp.py
@@ -9,24 +9,40 @@ class CheckIp:
         db = client.ips
         self.collection = db.list
 
-    def check_ip(self, proxy):
+    def check_ip(self, type, ip, port):
         ua = UserAgent()
         headers = ua.random
-        request = requests.get("https://www.baidu.com", headers=headers, proxy={proxy})
-        if request.status_code != 200:
+        print("正在检测：%s" % ip)
+        try:
+            request = requests.get("https://www.baidu.com", headers={'user-agent': headers},
+                                   proxies={'http': "%s://%s:%s" % (type, ip, port)}, timeout=2)
+            if request.status_code != 200:
+                print("请求失败，状态码：%d" % request.status_code)
+                return True
+            else:
+                print("请求成功，状态码：%d" % request.status_code)
+                return False
+        except:
+            print("请求异常")
             return True
-        else:
-            return False
 
     def get_ips(self):
+        del_count = 0
         ip_list = self.collection.find()
         if ip_list.count() > 0:
+            print("过滤开始")
             for item in list(ip_list):
-                proxy = "%s://%s:%s" % (item['type'], item['ip'], item['port'])
-                result = self.check_ip(proxy)
+                result = self.check_ip(item['type'].lower(), item['ip'], item['port'])
                 if result:
+                    print("正在清除")
                     self.collection.remove({'ip': item['ip']})
+                    del_count = del_count + 1
+                    print("已清除")
+        else:
+            print("无数据")
+        print("过滤结束,共清除：%d个IP" % del_count)
 
 
-checkIp = CheckIp()
-checkIp.get_ips()
+if __name__ == '__main__':
+    checkIp = CheckIp()
+    checkIp.get_ips()
